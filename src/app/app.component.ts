5049e22609bc1f7d553c340d2c9243af333adb77
Osama Soliman <drusama2@gmail.com>
2017-07-20 00:48:17 +0200

    adding plugin config and firing it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+
+//install steps https://ionicframework.com/docs/native/admob-free/
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -14,9 +18,9 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public admobFree: AdMobFree) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -29,11 +33,53 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.autoShowAds()
     });
+  }
+
+  autoShowAds() {
+    //interstitailConfig
+    const interstitailConfig: AdMobFreeInterstitialConfig = {
+      id: 'ca-app-pub-4457719262099261/9328226434',
+      isTesting: false,
+      autoShow: true
+    };
+    this.admobFree.interstitial.config(interstitailConfig);
+
+
+    //prepare
+    this.admobFree.interstitial.prepare()
+    .then(() => {
+        // banner Ad is ready
+        // if we set autoShow to false, then we will need to call the show method here
+    })
+    .catch(e => console.log(e));    
+    
+    
+
+    //bannerConfig
+    const bannerConfig: AdMobFreeBannerConfig = {
+      id: 'ca-app-pub-4457719262099261/7851493231',
+      isTesting: false,
+      autoShow: true
+    }
+    this.admobFree.banner.config(bannerConfig);
+
+    //prepare
+    this.admobFree.banner.prepare()
+    .then(() => {
+        // banner Ad is ready
+        // if we set autoShow to false, then we will need to call the show method here
+    })
+    .catch(e => console.log(e)); 
+    
+
   }
 
   openPage(page) {
